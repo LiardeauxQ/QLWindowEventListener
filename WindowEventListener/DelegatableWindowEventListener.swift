0b9e055763be3d07b924b06241f495c40be8c958
Quentin <quentin.liardeaux@epitech.eu>
2019-07-23 21:38:01 +0200

    Register delegates when added in order to remove them when remove function is called

--- WindowEventListener/DelegatableWindowEventListener.swift
+++ WindowEventListener/DelegatableWindowEventListener.swift
@@ -7,3 +7,97 @@
 //
 
 import Foundation
+
+private class DelegateInfo {
+    var delegate: ApplicationBundleDelegate
+    var bundleId: String
+    
+    init(_ delegate: ApplicationBundleDelegate, bundleId: String) {
+        self.delegate = delegate
+        self.bundleId = bundleId
+    }
+}
+
+public class DelegatableWindowEventListener: WindowEventListener {
+    private var applicationBundles: [DelegatableApplicationBundle]
+    private var waitingDelegates: [DelegateInfo]
+    private var registerDelegates: [DelegateInfo]
+
+    override public init() {
+        applicationBundles = []
+        waitingDelegates = []
+        registerDelegates = []
+        super.init()
+    }
+
+    override func fetchOpenApplications() {
+        let windowInfosRef = CGWindowListCopyWindowInfo(CGWindowListOption.optionOnScreenOnly, CGWindowID(0))
+        let windowInfos = windowInfosRef as? [[String: Any]] ?? []
+        
+        for windowInfo in windowInfos {
+            guard let pid = windowInfo["kCGWindowOwnerPID"] as? Int else {
+                continue
+            }
+            guard let bundle = applicationBundles.first(where: { $0.pid == pid}) else {
+                guard let app = try? DelegatableApplicationBundle(pid: pid),
+                    watchingBundleIdentifiers.contains(app.runningApp.bundleIdentifier ?? "") else {
+                        continue
+                }
+                if let window = BasicWindow(data: windowInfo) {
+                    app.addToBuffer(window: window)
+                }
+                applicationBundles.append(app)
+                continue
+            }
+            if watchingBundleIdentifiers.contains(bundle.runningApp.bundleIdentifier ?? ""),
+                let window = BasicWindow(data: windowInfo) {
+                bundle.addToBuffer(window: window)
+            }
+        }
+        updateDelegates()
+        applicationBundles.forEach({ $0.raiseEvents() })
+    }
+
+    public func add(delegate: ApplicationBundleDelegate, toBundleIdentifier bundleId: String) {
+
+        if registerDelegates.first(where: { $0.bundleId == bundleId }) != nil {
+            return
+        }
+
+        let delegateInfo = DelegateInfo(delegate, bundleId: bundleId)
+
+        registerDelegates.append(delegateInfo)
+        for bundle in applicationBundles {
+            guard bundle.runningApp.bundleIdentifier == bundleId else {
+                continue
+            }
+            bundle.delegate = delegate
+            return
+        }
+        waitingDelegates.append(delegateInfo)
+    }
+    
+    public func remove(delegateWithBundleId bundleId: String) {
+        guard let index = registerDelegates.firstIndex(where: { $0.bundleId == bundleId }) else {
+            return
+        }
+        for bundle in applicationBundles {
+            if bundle.runningApp.bundleIdentifier == registerDelegates[index].bundleId {
+                bundle.delegate = nil
+                break
+            }
+        }
+        registerDelegates.remove(at: index)
+    }
+
+    private func updateDelegates() {
+        var delegateBuffer: [DelegateInfo] = []
+
+        for _ in 0 ..< waitingDelegates.count {
+            delegateBuffer.append(waitingDelegates.removeFirst())
+        }
+        for value in delegateBuffer {
+            add(delegate: value.delegate, toBundleIdentifier: value.bundleId)
+        }
+    }
+}
--- WindowEventListener/PublishableWindowEventListener.swift
+++ WindowEventListener/PublishableWindowEventListener.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+public class PublisherWindowEventListener: WindowEventListener {
+    private var applicationBundles: [PublisherApplicationBundle]
+    
+    override public init() {
+        applicationBundles = []
+        super.init()
+    }
+    
+    override func fetchOpenApplications() {
+        let windowInfosRef = CGWindowListCopyWindowInfo(CGWindowListOption.optionOnScreenOnly, CGWindowID(0))
+        let windowInfos = windowInfosRef as? [[String: Any]] ?? []
+        
+        for windowInfo in windowInfos {
+            guard let pid = windowInfo["kCGWindowOwnerPID"] as? Int else {
+                continue
+            }
+            guard let bundle = applicationBundles.first(where: { $0.pid == pid}) else {
+                guard let app = try? PublisherApplicationBundle(pid: pid),
+                    watchingBundleIdentifiers.contains(app.runningApp.bundleIdentifier ?? "") else {
+                        continue
+                }
+                if let window = BasicWindow(data: windowInfo) {
+                    app.addToBuffer(window: window)
+                }
+                applicationBundles.append(app)
+                continue
+            }
+            if watchingBundleIdentifiers.contains(bundle.runningApp.bundleIdentifier ?? ""),
+                let window = BasicWindow(data: windowInfo) {
+                bundle.addToBuffer(window: window)
+            }
+        }
+        applicationBundles.forEach({ $0.raiseEvents() })
+    }
+}
